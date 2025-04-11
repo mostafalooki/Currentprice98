c6cff765a10b055f35645ca9cf946b927e1f2728
mostafalooki <irannot@gmail.com>
2025-04-11 16:55:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import asyncio
 import websockets
 import json
+import schedule
+import time
 from telegram import Bot
 from telegram.error import TelegramError
-from time import sleep
 
 # اطلاعات تلگرام (توکن ربات و آیدی کانال)
 TG_BOT_TOKEN = '7913278142:AAFreLlsi9pMI6zh8yxjSqT4ItOsHKUd1yk'
@@ -22,39 +23,58 @@ def send_to_telegram(message):
 
 # تابع برای دریافت قیمت تتر از نوبیتکس
 async def get_tether_price():
-    async with websockets.connect(WS_URL) as ws:
-        # اشتراک در کانال قیمت تتر
-        subscribe_message = {
-            "connect": {"name": "js"},
-            "subscribe": {
-                "channel": "public:orderbook-USDTIRT",  # کانال قیمت تتر
-                "recover": True,
-                "offset": 0,
-                "epoch": '0',
-                "delta": 'fossil'
-            },
-            "id": 4
-        }
+    while True:
+        try:
+            async with websockets.connect(WS_URL) as ws:
+                # اشتراک در کانال قیمت تتر
+                subscribe_message = {
+                    "connect": {"name": "js"},
+                    "subscribe": {
+                        "channel": "public:orderbook-USDTIRT",  # کانال قیمت تتر
+                        "recover": True,
+                        "offset": 0,
+                        "epoch": '0',
+                        "delta": 'fossil'
+                    },
+                    "id": 4
+                }
 
-        await ws.send(json.dumps(subscribe_message))  # ارسال پیام اشتراک
+                await ws.send(json.dumps(subscribe_message))  # ارسال پیام اشتراک
 
-        while True:
-            response = await ws.recv()
-            message = json.loads(response)
+                while True:
+                    # ارسال پینگ هر 30 ثانیه برای حفظ اتصال
+                    await ws.ping()
+                    await asyncio.sleep(30)
 
-            if 'id' in message and message['id'] == 4:
-                if 'subscribe' in message and 'publications' in message['subscribe']:
-                    publication = message['subscribe']['publications'][0]
-                    if publication.get('data'):
-                        data = json.loads(publication['data'])
-                        if data.get('asks') and len(data['asks']) > 0:
-                            tether_price = data['asks'][0][0]  # قیمت تتر
-                            print(f"Current Tether Price: {tether_price}")
-                            formatted_price = f"💵 قیمت تتر: {tether_price} تومان"
-                            send_to_telegram(formatted_price)  # ارسال قیمت به تلگرام
-            sleep(1)
+                    response = await ws.recv()
+                    message = json.loads(response)
 
-# اجرای برنامه
+                    if 'id' in message and message['id'] == 4:
+                        if 'subscribe' in message and 'publications' in message['subscribe']:
+                            publication = message['subscribe']['publications'][0]
+                            if publication.get('data'):
+                                data = json.loads(publication['data'])
+                                if data.get('asks') and len(data['asks']) > 0:
+                                    tether_price = data['asks'][0][0]  # قیمت تتر
+                                    print(f"Current Tether Price: {tether_price}")
+                                    formatted_price = f"💵 قیمت تتر: {tether_price} تومان"
+                                    send_to_telegram(formatted_price)  # ارسال قیمت به تلگرام
+                                    break  # پس از ارسال قیمت، اتصال WebSocket را قطع می‌کنیم
+
+        except websockets.exceptions.ConnectionClosedError as e:
+            print("WebSocket connection closed, reconnecting...")
+            await asyncio.sleep(5)  # تلاش مجدد برای اتصال بعد از 5 ثانیه
+
+# تابع ارسال قیمت به صورت دوره‌ای (هر دقیقه)
+def job():
+    print("Getting Tether price and sending to Telegram...")
+    asyncio.run(get_tether_price())
+
+# تنظیم برنامه برای ارسال قیمت هر 1 دقیقه
+schedule.every(1).minute.do(job)
+
+# اجرای برنامه به صورت مداوم
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_tether_price())
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
